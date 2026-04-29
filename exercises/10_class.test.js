ba811ba1aad2ffbef4eeca4dfd1ac5322b0739bb
albert-dev <al96@hotmail.es>
2026-04-29 10:47:57 +0200

    fix: solved test n.10

--- exercises/10_class.test.js
+++ exercises/10_class.test.js
@@ -2,7 +2,11 @@ test('10_class-1: has a constructor for initialization', () => {
   // Crea una classe Animal
   // Afegeix un constructor que prengui un paràmetre, el nom.
   // Assigna this.name al nom passat com a paràmetre
-
+  class Animal{
+    constructor (name){
+      this.name = name
+    }
+  }
   const animal = new Animal()
   const dog = new Animal('Dog')
 
@@ -13,17 +17,28 @@ test('10_class-1: has a constructor for initialization', () => {
 test('10_class-2: constructor can have default param values', () => {
   // Crea una classe Animal amb un constructor
   // Fes que la classe tingui un valor per defecte (utilitzant paràmetres per defecte) per al nom: 'Honey Badger'
-
+class Animal{
+    constructor (name = 'Honey Badger'){
+      this.name = name
+    }
+  }
   const animal = new Animal()
   const dog = new Animal('Dog')
-
+  
   expect(animal.name).toBe('Honey Badger')
   expect(dog.name).toBe('Dog')
 })
 
 test('10_class-3: can have instance methods', () => {
   // Crea una classe Animal, passa el nom al constructor i afegeix una funció sayName a la definició de la classe
-
+  class Animal{
+    constructor (name = 'Honey Badger'){
+      this.name = name
+    }
+    sayName(){
+      return  `My name is: ${this.name}`
+    }
+  }
   const animal = new Animal()
 
   expect(animal.sayName).toBeDefined()
@@ -35,6 +50,17 @@ test('10_class-4: can have static methods', () => {
   // Crea una classe Animal, passa el nom al constructor,
   // i afegeix un mètode estàtic create que prengui un nom i retorni una instància
 
+  class Animal{
+    constructor (name = 'Honey Badger'){
+      this.name = name
+    }
+       
+      static create(name)
+      {return new Animal(name)
+
+      }
+    }
+
   const animal = new Animal()
 
   expect(animal.create).toBeUndefined()
@@ -45,6 +71,22 @@ test('10_class-5: can extend another class', () => {
   // Crea una classe Animal
   // Crea una classe Dog que extengui Animal
   // Afegeix el mètode sayName a Dog
+
+  class Animal{
+    constructor (name){
+      this.name = name
+    }
+  }
+
+  class Dog extends Animal{
+    constructor(name){
+      super(name)
+    }
+
+    sayName(){
+      return this.name
+    }
+  }
 
   const dog = new Dog('Fido')
 
@@ -58,6 +100,14 @@ test('10_class-6: can use property setters and getters', () => {
   // Crea una classe Animal (no passis el nom al constructor)
   // Afegeix un setter per a la propietat name
   // Afegeix un getter per a la propietat name
+
+  class Animal{
+    #name
+    constructor (){
+    }
+    get name(){return `${this.#name} type of animal` }
+    set name(name) {this.#name = name}
+  }
 
   const animal = new Animal()
   animal.name = 'Dog'
